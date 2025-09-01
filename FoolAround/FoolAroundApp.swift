de412af4860eca5634d93a8ecc7d6a44e056288d
ken <jitamm20081@gmail.com>
2025-09-01 09:49:49 +0800

    Initial Commit

---
+++ FoolAround/FoolAroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoolAroundApp.swift
+//  FoolAround
+//
+//  Created by ken on 2025/9/1.
+//
+
+import SwiftUI
+
+@main
+struct FoolAroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
