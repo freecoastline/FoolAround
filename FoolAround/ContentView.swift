16cc35adc67c7a4d5fff0d066e90086e91584b49
ken <jitamm20081@gmail.com>
2025-09-02 08:55:52 +0800

    feat: add textLabel

--- FoolAround/ContentView.swift
+++ FoolAround/ContentView.swift
@@ -9,13 +9,13 @@ import SwiftUI
 
 struct ContentView: View {
     var timeLabel = DateFormat().formatDate(Date())
-
+    @State var inputText = ""
     var body: some View {
         VStack {
             Text(timeLabel)
                 .font(.footnote)
             Spacer()
-            
+            TextField("", text: $inputText)
         }
         .padding()
     }
